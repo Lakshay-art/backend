0ee90cf022d0e4cb3195d5c74274f60384bc6240
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2023-01-03 01:25:05 +0530

    cors error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const path =require("path");
  app.use(cors());
  const io=new Server(server,{
      cors:{
-         origin:"hybridKb.vercel.app"
+         origin:"http://localhost:3000/"
      }
  })
 
@@ -123,9 +123,9 @@ server.listen(port,()=>{
 // import express from "express";
 // const app = express() ;
 // const port = 9000;
-app.use("/",
-(req, res) => {res.json({ message: "Hello From Express App" });
-});
+// app.use("/",
+// (req, res) => {res.json({ message: "Hello From Express App" });
+// });
 // app. listen(9000, () = {
 // console. log('Starting Server on Port $(port]*');
 // 3);
